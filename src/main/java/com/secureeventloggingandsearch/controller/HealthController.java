a5e0ee1c84dd4d9c9bba726d3fc4d0c22e2c2f7d
Your Name <kosi53@hotmail.co.uk>
2026-05-04 12:18:16 +0100

    updated mapping for health controller

--- src/main/java/com/secureeventloggingandsearch/controller/HealthController.java
+++ src/main/java/com/secureeventloggingandsearch/controller/HealthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HealthController {
 
-    @GetMapping("/api/v1/health")
+    @GetMapping("/health")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
